0858220e46229eaf781dd6275aa52ae89f556657
Nuno Azevedo <feature.nuno@gmail.com>
2022-10-14 16:36:52 +0100

    fix: error on cordova load error.html
    Fixes the application crashing when webview redirects to error screen.
    
    This customisation has historically been in a patch in the template.
    Since we now have a fork, all customisations should be in the fork.
    
    References https://outsystemsrd.atlassian.net/browse/RNMT-1890
    References https://outsystemsrd.atlassian.net/browse/RNMT-5730
    References https://outsystemsrd.atlassian.net/browse/RNMT-6187
    
    (cherry picked from commit 728d2a7b7468e5dc7001e3a5af57d2f50d492293)

--- framework/src/org/apache/cordova/CordovaWebViewImpl.java
+++ framework/src/org/apache/cordova/CordovaWebViewImpl.java
@@ -267,6 +267,8 @@ public class CordovaWebViewImpl implements CordovaWebView {
             } else {
                 LOG.e(TAG, "Error loading url " + url, e);
             }
+        } catch (Exception e) {
+            LOG.e(TAG, "Error loading url " + url + " with error on file URI exposed", e);
         }
     }
 
